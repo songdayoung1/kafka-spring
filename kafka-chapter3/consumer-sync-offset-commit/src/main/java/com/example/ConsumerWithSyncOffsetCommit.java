5a644189bbb5ebf8fd74774e52f0f322c762c868
songdayoung <tjs2446@gmail.com>
2024-07-29 16:59:37 +0900

    [add] - 동기 오프셋 커밋 정리

--- kafka-chapter3/consumer-sync-offset-commit/src/main/java/com/example/ConsumerWithSyncOffsetCommit.java
+++ kafka-chapter3/consumer-sync-offset-commit/src/main/java/com/example/ConsumerWithSyncOffsetCommit.java
@@ -2,18 +2,22 @@ package com.example;
 
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ConsumerWithSyncCommit {
-    private final static Logger logger = LoggerFactory.getLogger(ConsumerWithSyncCommit.class);
+public class ConsumerWithSyncOffsetCommit {
+    private final static Logger logger = LoggerFactory.getLogger(ConsumerWithSyncOffsetCommit.class);
     private final static String TOPIC_NAME = "test";
     private final static String BOOTSTRAP_SERVERS = "3.137.41.97:9092";
     private final static String GROUP_ID = "test-group"; // 그룹 지정 
@@ -31,12 +35,17 @@ public class ConsumerWithSyncCommit {
 
         while (true) {
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
+            // commitSync가 파라미터로 받을 타입 선언 
+            Map<TopicPartition, OffsetAndMetadata> currentOffset = new HashMap<>();
+
             for (ConsumerRecord<String, String> record : records) {
                 logger.info("record:{}", record);
+                currentOffset.put(
+                        new TopicPartition(record.topic(), record.partition()),
+                        new OffsetAndMetadata(record.offset() + 1, null));
+                // currentOffset을 추가할 경우 특정 토픽, 파티션의 오프셋이 매번 커밋된다. 
+                consumer.commitSync(currentOffset);
             }
-            // poll 매서드 받은 가장 마지막 레코드의 오프셋 기준으로 커밋 실행
-            // 모든 레코드 처리가 끝난 이후 commitSync() 메서드 호출 
-            consumer.commitSync();
         }
     }
 }
